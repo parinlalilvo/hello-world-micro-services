ee060b3fc1d620407afdb32ca8be8a395cbba20a
pingprart <prart.srithananan@krungsri.com>
2019-08-21 17:00:27 +0700

    Refactor class name to the same class

--- src/main/kotlin/com/aycap/helloworld/HelloWorldApplication.kt
+++ src/main/kotlin/com/aycap/helloworld/HelloWorldApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class HelloWorldApplication
 
 fun main(args: Array<String>) {
-	runApplication<HelloWorldApplication>(*args)
+    runApplication<HelloWorldApplication>(*args)
 }
