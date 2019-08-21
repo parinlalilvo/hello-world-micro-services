298997e47ad8f8e1cb08201644f01bb3026264c7
pingprart <prart.srithananan@krungsri.com>
2019-08-21 17:25:37 +0700

    Convert data to json format

--- src/main/kotlin/com/aycap/helloworld/controller/HelloWorldController.kt
+++ src/main/kotlin/com/aycap/helloworld/controller/HelloWorldController.kt
@@ -1,5 +1,6 @@
 package com.aycap.helloworld.controller
 
+import com.aycap.helloworld.entity.Hello
 import com.aycap.helloworld.repository.HelloRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -11,8 +12,9 @@ class HelloWorldController(
         private val helloRepository: HelloRepository
 ) {
     @GetMapping(value = ["/hi"])
-    fun hello(): ResponseEntity<String> {
-        val hello = helloRepository.findById(1L).get()
-        return ResponseEntity(hello.name, HttpStatus.OK)
+    fun hello(): ResponseEntity<Hello> {
+        val optionalMessage = helloRepository.findById(1)
+        val result = optionalMessage.get()
+        return ResponseEntity(result, HttpStatus.OK)
     }
 }
