280f3f05d8fe791b39b97d53391de3d4f756de30
iLoneWolF <i.punnnnnn@gmail.com>
2019-08-21 16:38:44 +0700

    connect to db

--- init.sql
+++ init.sql
@@ -2,6 +2,6 @@ CREATE DATABASE hello;
 
 \c hello
 
-CREATE TABLE hello(name varchar(255));
+CREATE TABLE hello(id serial PRIMARY KEY,name varchar(255));
 
 INSERT INTO hello (name) VALUES ('hello world');
---
+++ src/main/kotlin/com/aycap/helloworld/repository/HelloRepository.kt
@@ -0,0 +1,6 @@
+package com.aycap.helloworld.repository
+
+import com.aycap.helloworld.entity.Hello
+import org.springframework.data.repository.CrudRepository
+
+interface HelloRepository : CrudRepository<Hello, Long>
