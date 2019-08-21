8df36924f2e5c3498e82f623391332dba04af45a
iLoneWolF <i.punnnnnn@gmail.com>
2019-08-21 14:44:50 +0700

    init spring-boot

---
+++ src/test/kotlin/com/aycap/helloworld/HelloworldApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.aycap.helloworld
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
