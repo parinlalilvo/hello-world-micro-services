521e7823b686cfe70dbccbcf51e6ee786cdf0df1
pingprart <prart.srithananan@krungsri.com>
2019-08-22 14:48:16 +0700

    Custom parameter

--- src/main/kotlin/com/aycap/helloworld/controller/HelloWorldController.kt
+++ src/main/kotlin/com/aycap/helloworld/controller/HelloWorldController.kt
@@ -11,9 +11,21 @@ import org.springframework.web.bind.annotation.RestController
 class HelloWorldController(
         private val helloService: HelloService
 ) {
-    @GetMapping(value = ["/hi"])
-    fun hello(): ResponseEntity<Hello> {
-        val result = helloService.getMessageById(1)
+    @GetMapping(value = ["/hiAll"])
+    fun getHelloAll(): ResponseEntity<MutableIterable<Hello>> {
+        val result = helloService.findAll()
+        return ResponseEntity(result, HttpStatus.OK)
+    }
+
+    @GetMapping(value = ["/hi"], params = ["id"])
+    fun getHelloById(id: Int): ResponseEntity<Hello> {
+        val result = helloService.findById(id)
+        return ResponseEntity(result, HttpStatus.OK)
+    }
+
+    @GetMapping(value = ["/hi"], params = ["name"])
+    fun getHelloByName(name: String): ResponseEntity<Hello> {
+        val result = helloService.findByName(name)
         return ResponseEntity(result, HttpStatus.OK)
     }
 }
--- src/main/kotlin/com/aycap/helloworld/repository/HelloRepository.kt
+++ src/main/kotlin/com/aycap/helloworld/repository/HelloRepository.kt
@@ -3,4 +3,6 @@ package com.aycap.helloworld.repository
 import com.aycap.helloworld.entity.Hello
 import org.springframework.data.repository.CrudRepository
 
-interface HelloRepository : CrudRepository<Hello, Long>
+interface HelloRepository : CrudRepository<Hello, Long> {
+    fun findByName(name: String): Hello
+}
